5783e97725594c3c80b53ab68ff05636d1f0ab5d
lawrence-le <lawrence_mklee@hotmail.com>
2025-02-04 21:27:32 +0800

    fix error

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: parseInt(process.env.PORT, 10) || 5173,
-    host: true, // Bind to all network interfaces
-  },
 });
