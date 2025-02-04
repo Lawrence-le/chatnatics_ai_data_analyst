0cb2a111f38de738813ec642a09376aa44ebf504
lawrence-le <lawrence_mklee@hotmail.com>
2025-02-04 21:34:46 +0800

    chore: clean services

--- frontend/src/service/apiUrl.js
+++ frontend/src/service/apiUrl.js
@@ -1,10 +1,3 @@
-// // export const apiGetUrl = import.meta.env.VITE_API_URL;
-// export const getColumnApi =
-//   "https://chatnatics-ai-data-analyst-backend.onrender.com/api/get_columns/";
-
-// export const getUserPromptApi =
-//   "https://chatnatics-ai-data-analyst-backend.onrender.com/api/user_prompt/prompt";
-
 export const getColumnApi = `${import.meta.env.VITE_API_URL}/api/get_columns/`;
 export const getUserPromptApi = `${
   import.meta.env.VITE_API_URL
