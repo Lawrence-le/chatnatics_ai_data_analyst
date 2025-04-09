f5615cbb2bfaaeae6a4429fe39226ed1ae489f2e
Lawrence-le <lawrence_mklee@hotmail.com>
2025-04-09 19:04:38 +0800

    debug: server_status not fetching

--- frontend/src/service/apiUrl.js
+++ frontend/src/service/apiUrl.js
@@ -6,9 +6,9 @@
 //   import.meta.env.VITE_API_URL
 // }/api/server_status`;
 
-const API_URL = import.meta.env.VITE_API_URL;
-// const API_URL = "http://localhost:5000";
+// const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = "http://localhost:5000";
 
 export const getColumnApi = `${API_URL}/api/get_columns/`;
 export const getUserPromptApi = `${API_URL}/api/user_prompt/prompt`;
-export const getServerStatusApi = `${API_URL}/api/server_status`;
+export const getServerStatusApi = `${API_URL}/api/server_status/`;
